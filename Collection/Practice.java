f2448f5de9d5212b7bfece6c3b23ca9c8dc5d978
Vanshika Poswal <228863839+vanshikaposwal@users.noreply.github.com>
2026-02-02 01:50:34 +0000

    Collection practice

---
+++ Collection/Practice.java
@@ -0,0 +1,27 @@
+package Collection;
+
+import java.util.*;
+
+public class Practice {
+    public static void main(String[] args) {
+        // Set
+        HashSet <Integer> set =new HashSet<>();
+
+        int[] arr = {1,2,3,8,4,1,5,7,3,2,2,2};
+        for(int i: arr){
+            set.add(i);
+        }
+
+        //  second largest 
+        int max = -1;
+        int secondMax = -1 ;
+            max = -1;
+        for(int i: set){
+            if(i>max){
+                secondMax = max;
+                max =i;
+            }
+        }
+        System.out.println(secondMax);
+    }
+}
