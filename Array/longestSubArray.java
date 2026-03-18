d225e070b45a80186b951179d13451d91f94f3ce
vanshikaposwal <228863839+vanshikaposwal@users.noreply.github.com>
2026-03-18 14:49:22 +0530

    Longest substring with sum K

--- Array/longestSubArray.java
+++ Array/longestSubArray.java
@@ -3,29 +3,50 @@ package Array;
 import java.util.Arrays;
 import java.util.HashMap;
 
-//public class longestSubArray {
-//    public static void main(String[] args){
-//        int[] arr = {-3, 2, 1};
-//        System.out.println(subarrayWithSumK(arr, 6));
-//    }
-//
-//    static int subarrayWithSumK(int[] arr, int k){
-//
-//        int left =0, right=0;
-//        int count =0;
-//        for(right=0; right< arr.length; right++){
-//            int sum=0;
-//            for(left=right; left < arr.length; left++){
-//                sum+= arr[left];
-//                if(sum == k){
-//                    int len = right-left+1;
-//                    if(len>count){
-//                        count = len;
-//                    }
-//                }else if(sum>k){
+public class longestSubArray {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,1,-1,1,1,2,0};
+        System.out.println(subarrayWithSumK(arr, 6));
+    }
+
+    static int subarrayWithSumK(int[] arr, int k){
+// ---------- Using Sliding window ----------------- only for positives...
+//        int left=0, size=0, sum=0;
+//        for(int right=0; right<arr.length; right++){
+//            sum+=arr[right];
+//            while(sum>k){
+//                sum-=arr[left];
+//                left++;
+//            }
+//            if(sum == k){
+//                size = Math.max(size, right-left+1);
 //            }
 //        }
-//
-//        return 0;
-//    }
-//}
+
+
+
+        //  ?????????  ... Sliding window does not work on negatives ....
+
+
+
+// ------------------------Using HashMap -----------------------------
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int size=0, sum=0;
+        for(int i=0; i<arr.length; i++){
+            sum+=arr[i];
+            if(sum == k){
+                size = i+1;
+            }
+            if(map.containsKey(sum -k )){
+                size = Math.max(size, i-(map.get(sum-k)));
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
+            }
+        }
+
+
+        return size;
+    }
+}
